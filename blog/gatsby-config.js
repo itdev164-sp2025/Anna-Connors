462004f794fc619e7e87bad20aa5365705c2ffa0
@Anna-Connors <connora2@gmatc.matc.edu>
2025-03-23 11:45:51 -0500

    Completed Activity 5

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -7,12 +7,12 @@ require('dotenv').config({
  */
 
 
-console.log("SPACE_ID:", process.env.CONTENTFUL_SPACE_ID),
-console.log("ACCESS_TOKEN:", process.env.CONTENTFUL_ACCESS_TOKEN),
+//console.log("SPACE_ID:", process.env.SPACE_ID);
+//console.log("ACCESS_TOKEN:", process.env.ACCESS_TOKEN);
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Blog`,
+    title: `A Blog of Many Things`,
     description: ``,
     author: `Anna Connors`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
